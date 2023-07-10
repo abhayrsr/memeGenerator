0c9d1d17036549ee26918bfa97066c8cee0e5008
abhay <abhayrajsinghrana123@gmail.com>
2023-07-10 22:10:53 +0530

    feat: react files

---
+++ memegenerator/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav.js';
+import Meme from './meme.js'
+
+
+function App() {
+  return (
+    <div className='App'>
+       <Nav />
+       <Meme />
+    </div>
+  );
+}
+
+export default App;
+ /* //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header> */
