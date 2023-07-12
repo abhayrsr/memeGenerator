cdf74498a54a466f456c2432e2e8a50e5e12c30c
abhay <abhayrajsinghrana123@gmail.com>
2023-07-12 13:11:44 +0530

    feat: useState

--- memegenerator/src/meme.js
+++ memegenerator/src/meme.js
@@ -2,11 +2,20 @@ import React from 'react';
 import memes from './memesData.js';
 
 export default function Meme(){
-    const [memeImg, setMemeImg] = React.useState("")
+    const [memeImg, setMemeImg] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/30b1gx.jpg"
+    })
+
+    const[allMemesImages, setAllMemesImages] = React.useState(memes)
     function getMemeImage(){
-        const memeDataArray = memes.data.memes
-        const randomNumber = Math.floor(Math.random() * memeDataArray.length)
-        setMemeImg(memeDataArray[randomNumber].url)
+        const memeArray = allMemesImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memeArray.length)
+        const url = memeArray[randomNumber].url
+        setMemeImg((prevMemeImg) => ({
+           ...prevMemeImg, randomImage: url
+        }))
     }
 
 
@@ -16,7 +25,7 @@ export default function Meme(){
                 <input className = "form-input" placeholder = "Top text" type = "text"></input>
                 <input className = "form-input" placeholder = "Bottom text" type = "text"></input>
                 <button className = "form-button" onClick = {getMemeImage}> Get a meme image </button>
-                <img src = {memeImg} className='form-image' alt ="memeImage"/>
+                <img src = {memeImg.randomImage} className='form-image' alt ="memeImage"/>
             </div>
         </main>
     )
