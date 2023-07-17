3664035c114dc38f7d0e906e2d06e9058ecc21a5
abhay <abhayrajsinghrana123@gmail.com>
2023-07-17 17:16:41 +0530

    feat: top/bottom text

--- memegenerator/src/meme.js
+++ memegenerator/src/meme.js
@@ -8,6 +8,18 @@ export default function Meme(){
         randomImage: "https://i.imgflip.com/30b1gx.jpg"
     })
 
+    function handleChange() {
+        const {name, value} = event.target
+
+        setMemeImg(prevMemeImg => {
+            return {
+                ...prevMemeImg,
+                [name]: value
+            }
+        })
+        
+    }
+
     const[allMemesImages, setAllMemesImages] = React.useState(memes)
     function getText(){
         
@@ -26,10 +38,33 @@ export default function Meme(){
     return(
         <main>
             <div className = "form">
-                <input className = "form-input" placeholder = "Top text" type = "text"></input>
-                <input className = "form-input" placeholder = "Bottom text" type = "text"></input>
-                <button className = "form-button" onClick = {getMemeImage}> Get a meme image </button>
-                <img src = {memeImg.randomImage} className='form-image' alt ="memeImage"/>
+                <input 
+                   className = "form-input" 
+                   placeholder = "Top text" 
+                   type = "text" 
+                   name = "topText"
+                   value = {memeImg.topText}
+                   onChange = {handleChange}
+                />
+
+                
+                <input 
+                   className = "form-input" 
+                   placeholder = "Bottom text" 
+                   type = "text"
+                   name = "bottomText"
+                   value = {memeImg.bottomText}
+                   onChange = {handleChange}
+                />
+
+                <button 
+                   className = "form-button" 
+                   onClick = {getMemeImage}> Get a meme image </button>
+                <div className = "meme">
+                   <img src = {memeImg.randomImage} className='form-image' alt ="memeImage"/>
+                   <h2 className = "meme--text top">{memeImg.topText}</h2>
+                   <h2 className = "meme--text bottom">{memeImg.bottomText}</h2>
+                </div>
             </div>
         </main>
     )
