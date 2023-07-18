9735a174fcf378c97eb6b091b3906049d1a2f89a
abhay <abhayrajsinghrana123@gmail.com>
2023-07-18 12:11:57 +0530

    feat: useEffect

--- memegenerator/src/meme.js
+++ memegenerator/src/meme.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import memes from './memesData.js';
 
 export default function Meme(){
     const [memeImg, setMemeImg] = React.useState({
@@ -8,7 +7,15 @@ export default function Meme(){
         randomImage: "https://i.imgflip.com/30b1gx.jpg"
     })
 
-    function handleChange() {
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemes(data.data.memes))
+    })
+
+    function handleChange(event) {
         const {name, value} = event.target
 
         setMemeImg(prevMemeImg => {
@@ -20,24 +27,27 @@ export default function Meme(){
         
     }
 
-    const[allMemesImages, setAllMemesImages] = React.useState(memes)
-    function getText(){
+    // const[allMemesImages, setAllMemesImages] = React.useState(memes)
+    // function getText(){
         
-    }
+    // }
 
     function getMemeImage(){
-        const memeArray = allMemesImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memeArray.length)
-        const url = memeArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMemeImg((prevMemeImg) => ({
            ...prevMemeImg, randomImage: url
         }))
+
+    //    const downloadImage = () => {
+    //         saveAs('url', 'image.jpg') // Put your image url here.
+    //     }
     }
 
 
     return(
         <main>
-            <div className = "form">
+            <div className = "form"> 
                 <input 
                    className = "form-input" 
                    placeholder = "Top text" 
@@ -59,12 +69,17 @@ export default function Meme(){
 
                 <button 
                    className = "form-button" 
-                   onClick = {getMemeImage}> Get a meme image </button>
+                   onClick = {getMemeImage}> Get a meme image 
+                </button>
                 <div className = "meme">
                    <img src = {memeImg.randomImage} className='form-image' alt ="memeImage"/>
                    <h2 className = "meme--text top">{memeImg.topText}</h2>
                    <h2 className = "meme--text bottom">{memeImg.bottomText}</h2>
                 </div>
+
+                {/* <div className = "download">
+                    <button onClick={getMemeImage.downloadImage}>Download</button>
+                </div> */}
             </div>
         </main>
     )
